501e635e54a6308564dc84f292d948545d00dca4
lhermosilloz <lhermosillo@pheratech.com>
2026-04-06 15:40:54 -0700

    video script update

--- videotak.sh
+++ videotak.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# videoTAK - Forwards voxl-streamer RTSP stream to OpenTAKServer                                                                                                                                                                                                                                                                                                                        CONFIG=/etc/telemtak/config.ini                                                                                                                                                             
-OTS_IP=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('$CONFIG'); print(c['tak']['host'])")
-STREAM=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('$CONFIG'); print(c['video']['stream_path'])")
-USERNAME=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('$CONFIG'); print(c['video']['username'])")
-PASSWORD=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('$CONFIG'); print(c['video']['password'])")
+# videoTAK - Forwards voxl-streamer RTSP stream to OpenTAKServer MediaMTX
+OTS_IP=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('/etc/telemtak/config.ini'); print(c['tak']['host'])")
+STREAM=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('/etc/telemtak/config.ini'); print(c['video']['stream_path'])")
+USERNAME=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('/etc/telemtak/config.ini'); print(c['video']['username'])")
+PASSWORD=$(python3 -c "import configparser; c=configparser.ConfigParser(); c.read('/etc/telemtak/config.ini'); print(c['video']['password'])")
 
 sleep 5
 
-exec ffmpeg -rtsp_transport tcp -i rtsp://localhost:8900/live -c copy -f rtsp rtsp://${USERNAME}:${PASSWORD}@${OTS_IP}:8554/${STREAM}
+exec ffmpeg \
+  -rtsp_transport tcp \
+  -i rtsp://localhost:8900/live \
+  -c copy \
+  -f rtsp \
+  rtsp://${USERNAME}:${PASSWORD}@${OTS_IP}:8554/${STREAM}
